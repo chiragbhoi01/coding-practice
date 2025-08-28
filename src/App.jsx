0ae1a041db0e4c2504bc34eefa6648df4999df4b
chiragbhoi01 <kumarchirag8890@gmail.com>
2025-08-28 23:37:15 +0530

    Day 28 React Context Penidin refactor: comment out unused components in App.jsx for cleaner code

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,38 @@
 // import Parent from './components/Day11/Parent.tsx'
 // import GetData from './components/Task/UserCard.tsx'
 // import Parent from './components/Day26/Parent'
-import CustomerDetails from './components/Day27/CustomerDetails'
+// import CustomerDetails from './components/Day27/CustomerDetails'
+// import TodoApp from './components/Day28/TodoApp'
+import ThemeProvider from './components/Day28/ThemeProvider'
+import ThemeSwitcher from './components/Day28/ThemeSwitcher'
 
 function App() {
-  
+
 
   return (
     <>
-    {/* <Usestate /> */}
-    {/* <Day3/> */}
-    {/* <Day4/> */}
-    {/* <Day5/> */}
-    {/* <Day6/> */}
-    {/* <Day7/> */}
-    {/* <Day8/> */}
-    {/* <Day9/> */}
-    {/* <Day10/> */}
-    {/* <Parent/> */}
-    {/* <GetData/> */}
-    {/* <UserCard/> */}
-    {/* <ParentCard/> */}
-    {/* <Parent/> */}
-    <CustomerDetails/>
+      {/* <Usestate /> */}
+      {/* <Day3/> */}
+      {/* <Day4/> */}
+      {/* <Day5/> */}
+      {/* <Day6/> */}
+      {/* <Day7/> */}
+      {/* <Day8/> */}
+      {/* <Day9/> */}
+      {/* <Day10/> */}
+      {/* <Parent/> */}
+      {/* <GetData/> */}
+      {/* <UserCard/> */}
+      {/* <ParentCard/> */}
+      {/* <Parent/> */}
+      {/* <CustomerDetails/> */}
+      <TodoApp />
+      <ThemeProvider>
+        
+      </ThemeProvider>
 
-    
-    
+
+
 
     </>
   )
